d1ba86e3314aaa0907f6356ddd4613a39cb99770
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-10-31 19:15:24 +0100

    import chess pieces

--- rusty_chess_ui/src/routes/+page.svelte
+++ rusty_chess_ui/src/routes/+page.svelte
@@ -6,6 +6,17 @@
   let reversed = false;
   let botState = "off";
 
+  let board = [
+    ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+    ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+    ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+  ];
+
   function generate_series(n: number) {
     return Array.from({ length: n }, (_, i) => i);
   }
@@ -17,8 +28,13 @@
       <div class="row">
         {#each reversed ? generate_series(8).reverse() : generate_series(8) as col}
           <div class="square">
-            <div class={(row + col) % 2 == 0 ? "light-square" : "dark-square"}>
-              {row + col}
+            <div class={(row + col) % 2 == 0 ? "dark-square" : "light-square"}>
+              {#if board[row][col] !== ""}
+                <img
+                  src={`../src/assets/${board[row][col]}.svg`}
+                  alt="A chess piece"
+                />
+              {/if}
             </div>
           </div>
         {/each}
