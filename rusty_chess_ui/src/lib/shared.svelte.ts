a6007a190cb6d5b6c06c1b8e35a1ed99620b6678
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-11-14 21:31:37 +0100

    split interfaces into backend and frontend

--- rusty_chess_ui/src/lib/shared.svelte.ts
+++ rusty_chess_ui/src/lib/shared.svelte.ts
@@ -1,3 +1,27 @@
+const pieceFromString: (x: string) => ChessPiece = (pieceString) => {
+    const pieceTable: Record<string, ChessPiece> = {
+        "wR": { piece_type: "Rook", color: "White" },
+        "wN": { piece_type: "Knight", color: "White" },
+        "wB": { piece_type: "Bishop", color: "White" },
+        "wQ": { piece_type: "Queen", color: "White" },
+        "wK": { piece_type: "King", color: "White" },
+        "wP": { piece_type: "Pawn", color: "White" },
+        "bR": { piece_type: "Rook", color: "Black" },
+        "bN": { piece_type: "Knight", color: "Black" },
+        "bB": { piece_type: "Bishop", color: "Black" },
+        "bQ": { piece_type: "Queen", color: "Black" },
+        "bK": { piece_type: "King", color: "Black" },
+        "bP": { piece_type: "Pawn", color: "Black" },
+    }
+
+    return pieceTable[pieceString] ?? undefined
+}
+
+const pieceToString: (x: ChessPiece) => String = (piece) => {
+    return `${piece.color[0]}${piece.piece_type[0]}`
+}
+
+
 let boardPre = [
     ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
     ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
@@ -9,12 +33,15 @@ let boardPre = [
     ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
 ];
 
-export let board: { id: number; content: string; isDndShadowItem?: boolean }[][][] = $state(
-    boardPre.map((x, row) => {
-        return x.map((elem, col) => {
-            return elem === "" ? [] : [{ id: row * 8 + col, content: elem }]
-        })
-    })
+export let board: Board = $state(
+    {
+        board: boardPre.map((x, row) => {
+            return x.map((elem, col) => {
+                return elem === "" ? [] : [{ id: row * 8 + col, piece: pieceFromString(elem) }]
+            })
+        }),
+        turn: "White"
+    }
 )
 
 console.log($state.snapshot(board))
