c3161ff4dafd11aa35f9900dcc71361bab35e74f
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-12-08 21:26:58 +0100

    Disable toggling bot on promoting

--- rusty_chess_ui/src/routes/+page.svelte
+++ rusty_chess_ui/src/routes/+page.svelte
@@ -57,9 +57,9 @@
       {/each}
     </div>
 
-    <div class="grid gap-4 w-64 ml-4">
+    <div class="grid gap-4 w-64 ml-4 grid-rows-[1fr_1fr_2fr_1fr_1fr]">
       <button
-        class="bg-gray-500 border-2 border-gray-700 rounded-lg py-2 px-4 hover:border-gray-400"
+        class="bg-gray-500 border-2 border-gray-700 rounded-lg px-4 hover:border-gray-400"
         onclick={() => {
           reversed = !reversed;
         }}
@@ -68,10 +68,11 @@
       </button>
 
       <button
-        class="bg-gray-500 border-2 border-gray-700 rounded-lg py-2 px-4 hover:border-gray-400"
+        class="bg-gray-500 border-2 border-gray-700 rounded-lg px-4 hover:border-gray-400"
         onclick={async () => {
           reversed = false;
           turnState.restartGame();
+          promotionState.promotionData = null;
           board.restart();
           restartGameState();
         }}
@@ -80,72 +81,72 @@
       </button>
 
       {#if promotionState.isPromoting}
-        <div
-          class="text-gray-400 rounded-lg flex items-center justify-center py-2 row-span-2"
-        >
-          <button onclick={async () => await promotePawn("Queen")}>
-            <img
-              src={`../src/assets/${turnState.color === "White" ? "wQ" : "bQ"}.svg`}
-              alt="A chess piece"
-              class="w-full h-full"
-            />
-          </button>
-          <button onclick={async () => await promotePawn("Rook")}>
-            <img
-              src={`../src/assets/${turnState.color === "White" ? "wR" : "bR"}.svg`}
-              alt="A chess piece"
-              class="w-full h-full"
-            />
-          </button>
-          <button onclick={async () => await promotePawn("Bishop")}>
-            <img
-              src={`../src/assets/${turnState.color === "White" ? "wB" : "bB"}.svg`}
-              alt="A chess piece"
-              class="w-full h-full"
-            />
-          </button>
-          <button onclick={async () => await promotePawn("Knight")}>
-            <img
-              src={`../src/assets/${turnState.color === "White" ? "wN" : "bN"}.svg`}
-              alt="A chess piece"
-              class="w-full h-full"
-            />
-          </button>
+        <div class="rounded-lg flex items-center justify-center">
+          <div>
+            <button onclick={async () => await promotePawn("Queen")}>
+              <img
+                src={`../src/assets/${turnState.color === "White" ? "wQ" : "bQ"}.svg`}
+                alt="A chess piece"
+                class="w-full h-full"
+              />
+            </button>
+            <button onclick={async () => await promotePawn("Rook")}>
+              <img
+                src={`../src/assets/${turnState.color === "White" ? "wR" : "bR"}.svg`}
+                alt="A chess piece"
+                class="w-full h-full"
+              />
+            </button>
+            <button onclick={async () => await promotePawn("Bishop")}>
+              <img
+                src={`../src/assets/${turnState.color === "White" ? "wB" : "bB"}.svg`}
+                alt="A chess piece"
+                class="w-full h-full"
+              />
+            </button>
+            <button onclick={async () => await promotePawn("Knight")}>
+              <img
+                src={`../src/assets/${turnState.color === "White" ? "wN" : "bN"}.svg`}
+                alt="A chess piece"
+                class="w-full h-full"
+              />
+            </button>
+          </div>
         </div>
       {:else if (turnState.turn as CurrentPlayer) === "white" || (turnState.turn as CurrentPlayer) === "whiteBot"}
         <div
-          class="bg-gray-300 text-black rounded-lg flex items-center justify-center py-2 row-span-2"
+          class="bg-gray-300 text-black rounded-lg flex items-center justify-center"
         >
           White's turn
         </div>
       {:else if (turnState.turn as CurrentPlayer) === "black" || (turnState.turn as CurrentPlayer) === "blackBot"}
         <div
-          class="bg-black text-gray-400 rounded-lg flex items-center justify-center py-2 row-span-2"
+          class="bg-black text-gray-400 rounded-lg flex items-center justify-center"
         >
           Black's turn
         </div>
       {:else if (turnState.turn as { endgameMsg: string }).endgameMsg}
-        <div
-          class="text-gray-400 rounded-lg flex items-center justify-center py-2 row-span-2"
-        >
+        <div class="text-gray-400 rounded-lg flex items-center justify-center">
           {(turnState.turn as { endgameMsg: string }).endgameMsg}
         </div>
       {/if}
 
       <button
-        class="bg-gray-500 border-2 border-gray-700 rounded-lg py-2 px-4 hover:border-gray-400"
+        class="bg-gray-500 border-2 border-gray-700 rounded-lg px-4 hover:border-gray-400 disabled:opacity-50"
         onclick={async () => {
           await turnState.toggleWhiteBot();
         }}
+        disabled={promotionState.isPromoting}
       >
         White's bot ({turnState.whiteBotState})
       </button>
 
       <button
-        class="bg-gray-500 border-2 border-gray-700 rounded-lg py-2 px-4 hover:border-gray-400"
+        class="bg-gray-500 border-2 border-gray-700 rounded-lg px-4 hover:border-gray-400 disabled:opacity-50"
         onclick={async () => {
           await turnState.toggleBlackBot();
         }}
+        disabled={promotionState.isPromoting}
       >
         Black's bot ({turnState.blackBotState})
       </button>
