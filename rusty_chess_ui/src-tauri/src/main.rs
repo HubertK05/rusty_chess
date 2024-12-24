48b659618d52f6e97db9cbe5ce4ff3ebff1f1fcf
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-12-24 20:59:39 +0100

    Fix file not found bug in development

--- rusty_chess_ui/src-tauri/src/main.rs
+++ rusty_chess_ui/src-tauri/src/main.rs
@@ -2,11 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Disables the default renderer, which doesn't work with NVIDIA GPU on Linux
+    // https://github.com/tauri-apps/tauri/issues/9304
     #[cfg(unix)]
-    {
-        let env = include_str!("../.env");
-        dotenvy::from_read(env.as_bytes()).unwrap();
-    }
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
 
     rusty_chess_ui_lib::run()
 }
