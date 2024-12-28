93802f1f3e71c31a2b8f9da054063f6cdff69246
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-12-28 12:31:58 +0100

    Remove OnceLocks

--- rusty_chess_ui/src-tauri/src/lib.rs
+++ rusty_chess_ui/src-tauri/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::BTreeMap,
-    sync::{Arc, OnceLock},
-};
+use std::{collections::BTreeMap, sync::Arc};
 
 use backend::{
     board_setup::models::{Board, FenNotation},
@@ -22,9 +19,9 @@ use tokio::sync::Notify;
 struct AppState {
     board: Mutex<Board>,
     repetition_map: Mutex<BTreeMap<u64, u8>>,
-    opening_book: OnceLock<OpeningBook>,
-    app_settings: OnceLock<Mutex<AppSettings>>,
-    turn_counter: OnceLock<Arc<Mutex<u32>>>,
+    opening_book: OpeningBook,
+    app_settings: Mutex<AppSettings>,
+    turn_counter: Arc<Mutex<u32>>,
     toggled: Arc<Mutex<ToggleState>>,
     cvar: Notify,
 }
@@ -79,7 +76,7 @@ impl AppState {
                 .map_err(|e| format!("Failed to send end-game event: {e:?}"))?;
         }
 
-        *self.turn_counter.get().unwrap().lock().await += 1;
+        *self.turn_counter.lock().await += 1;
         *self.toggled.lock().await = ToggleState::Idle;
         self.cvar.notify_waiters();
         Ok(())
@@ -124,7 +121,7 @@ impl AppState {
         *self.board.lock().await = Board::new_game();
         *self.repetition_map.lock().await = BTreeMap::new();
         *self.toggled.lock().await = ToggleState::Idle;
-        *self.turn_counter.get().unwrap().lock().await += 1;
+        *self.turn_counter.lock().await += 1;
         self.cvar.notify_waiters();
     }
 }
@@ -150,26 +147,25 @@ async fn autoplay_move(
     let board = board_guard.clone();
     drop(board_guard);
 
-    let starting_turn_count = { *state.turn_counter.get().unwrap().lock().await };
+    let starting_turn_count = { *state.turn_counter.lock().await };
 
-    let chosen_move =
-        if let Some(move_vec) = state.opening_book.get().unwrap().0.get(&fen.to_draw_fen()) {
-            let mut rng = thread_rng();
-            let san = move_vec
-                .choose_weighted(&mut rng, |(_, popularity)| *popularity)
-                .unwrap();
-            let res = parse_move(fen, san.0.clone()).expect("cannot parse move");
+    let chosen_move = if let Some(move_vec) = state.opening_book.0.get(&fen.to_draw_fen()) {
+        let mut rng = thread_rng();
+        let san = move_vec
+            .choose_weighted(&mut rng, |(_, popularity)| *popularity)
+            .unwrap();
+        let res = parse_move(fen, san.0.clone()).expect("cannot parse move");
 
-            Some(res)
-        } else {
-            let repetition_map = state.repetition_map.lock().await.clone();
-            let app_settings = { state.app_settings.get().unwrap().lock().await.clone() };
+        Some(res)
+    } else {
+        let repetition_map = state.repetition_map.lock().await.clone();
+        let app_settings = { state.app_settings.lock().await.clone() };
 
-            let thread = std::thread::spawn(move || {
-                backend::chess_bot::choose_move(&board, repetition_map, app_settings)
-            });
-            thread.join().unwrap()
-        };
+        let thread = std::thread::spawn(move || {
+            backend::chess_bot::choose_move(&board, repetition_map, app_settings)
+        });
+        thread.join().unwrap()
+    };
 
     let Some(chosen_move) = chosen_move else {
         return Err("Failed to choose move".into());
@@ -181,7 +177,7 @@ async fn autoplay_move(
         println!("Notified");
     }
 
-    let is_redundant = { *state.turn_counter.get().unwrap().lock().await != starting_turn_count };
+    let is_redundant = { *state.turn_counter.lock().await != starting_turn_count };
 
     if is_redundant {
         return Ok(CancelResult::Canceled);
@@ -218,61 +214,45 @@ async fn update_settings(
     state: tauri::State<'_, AppState>,
     new_settings: AppSettings,
 ) -> Result<(), ()> {
-    *state.app_settings.get().unwrap().lock().await = new_settings;
+    *state.app_settings.lock().await = new_settings;
     Ok(())
 }
 
 #[tauri::command]
 async fn get_settings(state: tauri::State<'_, AppState>) -> Result<AppSettings, ()> {
-    return Ok(*state.app_settings.get().unwrap().lock().await);
+    return Ok(*state.app_settings.lock().await);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .manage(AppState {
-            board: Mutex::new(Board::new_game()),
-            repetition_map: Mutex::new(BTreeMap::new()),
-            opening_book: OnceLock::new(),
-            app_settings: OnceLock::new(),
-            turn_counter: OnceLock::new(),
-            toggled: Arc::new(Mutex::new(ToggleState::Idle)),
-            cvar: Notify::new(),
-        })
         .setup(|app| {
             let opening_book_path = app
                 .path()
                 .resolve("resources/opening_book.txt", BaseDirectory::Resource)
                 .unwrap();
+            let opening_book =
+                OpeningBook::from_file(opening_book_path.as_os_str().to_str().unwrap());
 
             let settings_path = app
                 .path()
                 .resolve("resources/settings.toml", BaseDirectory::Resource)
                 .unwrap();
+            let settings =
+                AppSettings::get_from_file(settings_path.as_os_str().to_str().unwrap()).unwrap();
 
-            app.state::<AppState>()
-                .opening_book
-                .set(OpeningBook::from_file(
-                    opening_book_path.as_os_str().to_str().unwrap(),
-                ))
-                .unwrap();
+            let toggled = Arc::new(Mutex::new(ToggleState::Idle));
+            app.manage(AppState {
+                board: Mutex::new(Board::new_game()),
+                repetition_map: Mutex::new(BTreeMap::new()),
+                opening_book,
+                app_settings: Mutex::new(settings),
+                turn_counter: Arc::new(Mutex::new(0)),
+                toggled: toggled.clone(),
+                cvar: Notify::new(),
+            });
 
-            app.state::<AppState>()
-                .app_settings
-                .set(Mutex::new(
-                    AppSettings::get_from_file(settings_path.as_os_str().to_str().unwrap())
-                        .unwrap(),
-                ))
-                .unwrap();
-
-            let counter = Arc::new(Mutex::new(0));
-            app.state::<AppState>()
-                .turn_counter
-                .set(Arc::clone(&counter))
-                .expect("Attempted to set the channel more than once");
-
-            let toggled = app.state::<AppState>().toggled.clone();
             app.listen("cancel-move", move |_| {
                 let toggled = toggled.clone();
                 spawn(async move {
